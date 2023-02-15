bff27ed1cd86f510e270912857d6085d6cf9c813
akash-singha-3558 <akashsingha@gmail.com>
2023-02-16 02:32:22 +0530

    footer and contact added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,34 @@
 import { Box, Center, Text } from '@chakra-ui/react'
 import React from 'react'
+import { AiFillGithub ,AiFillLinkedin,AiOutlineMail,AiOutlinePhone} from "react-icons/ai";
 
 export const Contact = () => {
   return (
     <Box  
-    h="100vh"
+    h="auto"
     bg="black"
-    w="100%" id="contact"   color="white" fontFamily="Roboto Mono, monospace">
+    w="100%" id="contact"   color="white" fontFamily="Roboto Mono, monospace" p="20px">
     
     <Center> <Text  borderBottom="2px solid teal" p="5px" fontSize="22px" mt="100px">Get In Touch</Text></Center>
-    
+    <Box w={{ base:"80%",
+    sm:"80%",
+    md:"80%",
+    lg:"45%",
+    xl:"45%",
+    "2xl":"45%"}} m="auto"  color="#8993b1" mt="20px">
+    <Text textAlign={"center"}>I love to build web applications from scratch.If you want to collaborate or have some work for me, get in touch and share me your thoughts. — I can’t wait to hear all about it! 😇</Text>
+    </Box>
+    <Box display="flex"    flexWrap={"wrap"}  justifyContent="center"  margin="auto" alignItems={"center"} gap="10px" mt="20px">
+    <Box display="flex" justifyContent={"center"}  w="100%" gap="5px" alignItems={"center"}><AiOutlinePhone     fontSize={"40px"} cursor="pointer" onClick={()=>{window.open("tel:123-456-7890","_blank")}}/>  <Text id="contact-phone" color="#8993b1">8918751633</Text></Box>
+<Box display="flex" justifyContent={"center"}  w="100%" gap="5px" alignItems={"center"}>< AiFillGithub   fontSize={"40px"} cursor="pointer" onClick={()=>{window.open("https://github.com/akash-singha-3558","_blank")}}/><Text onClick={()=>{window.open("https://github.com/akash-singha-3558","_blank")}} color="#8993b1" id="contact-github"  cursor={"pointer"}>@akash-singha-3558</Text></Box>
+<Box display="flex" justifyContent={"center"}  w="100%" gap="5px" alignItems={"center"}><AiFillLinkedin   fontSize={"40px"} cursor="pointer" onClick={()=>{window.open("https://www.linkedin.com/in/akash-singha-b62992213/","_blank")}}/><Text onClick={()=>{window.open("https://www.linkedin.com/in/akash-singha-b62992213/","_blank")}} id="contact-linkedin" color="#8993b1" cursor={"pointer"}>@akash-singha-b62992213</Text></Box>
+   
+ <Box display="flex" justifyContent={"center"}  w="100%" gap="5px" alignItems={"center"}><AiOutlineMail    fontSize={"40px"} cursor="pointer" onClick={()=>{window.open("mailto:akashsingha3558@gmail.com","_blank")}}/>  <Text onClick={()=>{window.open("mailto:akashsingha3558@gmail.com","_blank")}} color="#8993b1" cursor={"pointer"} id="contact-email">akashsingha3558@gmail.com</Text></Box>
+
+   
+    </Box>
+<hr  style={{color:"grey",background:"grey",width:"70%",margin:"auto",marginTop:"20px"}}/>
+<Text textAlign={"center"} mt="5px" color="#8993b1">Designed and build by Akash Singha, 2023 All rights reserved.</Text>
     </Box>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,22 +27,22 @@ export const Skills = () => {
     >
        <Center> <Text  borderBottom="2px solid teal" p="5px" fontSize="22px" mt="100px">Skills</Text></Center>
        <Box display="flex" w="80%" m="auto" flexWrap={"wrap"} justifyContent="space-evenly" mt="20px" gap="40px" >
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={HTML} alt="html"/><Text textAlign={"center"} className="skills-card-name">HTML</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={CSS} alt="css"/><Text textAlign={"center"} className="skills-card-name">CSS</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={JSS} alt="javascript"/><Text textAlign={"center"} className="skills-card-name">JavaScript</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={Reacts} alt="react"/><Text textAlign={"center"} className="skills-card-name">React</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={HTML} alt="html"/><Text textAlign={"center"} className="skills-card-name">HTML</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={CSS} alt="css"/><Text textAlign={"center"} className="skills-card-name">CSS</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={JSS} alt="javascript"/><Text textAlign={"center"} className="skills-card-name">JavaScript</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={Reacts} alt="react"/><Text textAlign={"center"} className="skills-card-name">React</Text></Box>
 
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={redux} alt="redux"/><Text textAlign={"center"} className="skills-card-name">Redux</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ts} alt="typescript"/><Text textAlign={"center"} className="skills-card-name">TypeScript</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={redux} alt="redux"/><Text textAlign={"center"} className="skills-card-name">Redux</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ts} alt="typescript"/><Text textAlign={"center"} className="skills-card-name">TypeScript</Text></Box>
        
-       <Box   className="skills-card" border="4px solid grey"  borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img"h="92px" w="92px" src={next} alt="Next js"/><Text  textAlign={"center"} className="skills-card-name">Next js</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1"  borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img"h="92px" w="92px" src={next} alt="Next js"/><Text  textAlign={"center"} className="skills-card-name">Next js</Text></Box>
 
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={chakraui} alt="Chakra Ui"/><Text textAlign={"center"} className="skills-card-name" >Chakra-UI</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={nodes} alt="NodeJS"/><Text textAlign={"center"} className="skills-card-name" >NodeJS</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={express} alt="ExpressJs"/><Text textAlign={"center"} className="skills-card-name" >ExpressJs</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={mongo} alt="MongoDB"/><Text textAlign={"center"} className="skills-card-name" >MongoDB</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ gitbash } alt="GitBash"/><Text textAlign={"center"} className="skills-card-name" >GitBash</Text></Box>
-       <Box   className="skills-card" border="4px solid grey" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ GitHub } alt="GitHub"/><Text textAlign={"center"} className="skills-card-name" >GitHub</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={chakraui} alt="Chakra Ui"/><Text textAlign={"center"} className="skills-card-name" >Chakra-UI</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={nodes} alt="NodeJS"/><Text textAlign={"center"} className="skills-card-name" >NodeJS</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={express} alt="ExpressJs"/><Text textAlign={"center"} className="skills-card-name" >ExpressJs</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={mongo} alt="MongoDB"/><Text textAlign={"center"} className="skills-card-name" >MongoDB</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ gitbash } alt="GitBash"/><Text textAlign={"center"} className="skills-card-name" >GitBash</Text></Box>
+       <Box   className="skills-card" border="4px solid #8993b1" borderRadius="20px" p="20px" pl="30px" pr="30px"><Image className="skills-card-img" src={ GitHub } alt="GitHub"/><Text textAlign={"center"} className="skills-card-name" >GitHub</Text></Box>
  
 
 
